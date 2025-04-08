06840c301471049ffeedb81b63efc390e9f47de8
samuel150a <samuelofficial184@gmail.com>
2025-04-08 22:05:48 +0530

    init commit

---
+++ Temple1/src/com/temple/external/Temple.java
@@ -0,0 +1,11 @@
+package com.temple.external;
+
+public class Temple {
+    public void tempdisp() {
+        System.out.println("running temple in temple method");
+    }
+        public void show() {
+            System.out.println("calling this method from NGO");
+        }
+    }
+
