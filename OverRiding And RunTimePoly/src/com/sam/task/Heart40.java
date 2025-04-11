7c9e0510c2ed1aaea72b591637fb0985d9aefe7a
samuel150a <samuelofficial184@gmail.com>
2025-04-11 10:27:05 +0530

    init commit

---
+++ OverRiding And RunTimePoly/src/com/sam/task/Heart40.java
@@ -0,0 +1,11 @@
+package com.sam.task;
+
+public class Heart40 {
+    public Heart40() {
+        System.out.println("Running in the Heart40 constructor");
+    }
+
+    public void beat() {
+        System.out.println("Running in the public method beat");
+    }
+}
---
+++ OverRiding And RunTimePoly/src/com/sam/task/RunnerHeart40.java
@@ -0,0 +1,15 @@
+package com.sam.task;
+
+public class RunnerHeart40 {
+    public static void main(String[] args) {
+        Heart40 heart = new Heart40();
+        heart.beat();
+
+        Heart40 h = new Love();
+        h.beat();
+
+        Love love = new Love();
+        love.care();
+        love.feel();
+    }
+}
