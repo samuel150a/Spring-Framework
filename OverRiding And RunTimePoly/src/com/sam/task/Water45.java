5936db2699e58b8e7b92e9491b12a31aa50c93b7
samuel150a <samuelofficial184@gmail.com>
2025-04-11 10:37:10 +0530

    init commit

---
+++ OverRiding And RunTimePoly/src/com/sam/task/Water45.java
@@ -0,0 +1,11 @@
+package com.sam.task;
+
+public class Water45 {
+    public Water45() {
+        System.out.println("Running in the Water45 constructor");
+    }
+
+    public void flow() {
+        System.out.println("Running in the public method flow");
+    }
+}
