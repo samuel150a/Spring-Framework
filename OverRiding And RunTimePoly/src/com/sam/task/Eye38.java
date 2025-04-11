ab6149e46e36bb30f435e347c7ee27bd4d2717f8
samuel150a <samuelofficial184@gmail.com>
2025-04-11 10:22:15 +0530

    init commit

---
+++ OverRiding And RunTimePoly/src/com/sam/task/Eye38.java
@@ -0,0 +1,11 @@
+package com.sam.task;
+
+public class Eye38 {
+    public Eye38() {
+        System.out.println("Running in the Eye38 constructor");
+    }
+
+    public void see() {
+        System.out.println("Running in the public method see");
+    }
+}
---
+++ OverRiding And RunTimePoly/src/com/sam/task/RunnerEye38.java
@@ -0,0 +1,15 @@
+package com.sam.task;
+
+public class RunnerEye38 {
+    public static void main(String[] args) {
+        Eye38 eye = new Eye38();
+        eye.see();
+
+        Eye38 e = new Vision();
+        e.see();
+
+        Vision vision = new Vision();
+        vision.focus();
+        vision.observe();
+    }
+}
