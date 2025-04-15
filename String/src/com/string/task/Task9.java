4a3ff4180bedb8ed0ee03f3b8e6323985aaa4839
samuel150a <samuelofficial184@gmail.com>
2025-04-15 12:42:25 +0530

    init commit

---
+++ String/src/com/string/task/Task9.java
@@ -0,0 +1,13 @@
+package com.string.task;
+
+public class Task9 {
+    public static void main(String[] args) {
+        String str = "madam";
+        if (str.equals("madam")) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+}
+
