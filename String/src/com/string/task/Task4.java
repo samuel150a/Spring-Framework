326b8f8a3596945481266bd2f162abc4059cc581
samuel150a <samuelofficial184@gmail.com>
2025-04-15 12:32:20 +0530

    init commit

---
+++ String/src/com/string/task/Task4.java
@@ -0,0 +1,11 @@
+package com.string.task;
+
+public class Task4 {
+
+
+    public static void main(String[] args) {
+        String sentence = "Hello, how are you?";
+        int characterCount = sentence.length();
+        System.out.println("Number of characters: " + characterCount);
+    }
+}
