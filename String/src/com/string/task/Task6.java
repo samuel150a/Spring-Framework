6a00da08f81afa5953b74c1f8a36436400ec1004
samuel150a <samuelofficial184@gmail.com>
2025-04-15 12:36:18 +0530

    init commit

---
+++ String/src/com/string/task/Task6.java
@@ -0,0 +1,9 @@
+package com.string.task;
+
+public class Task6 {
+    public static void main(String[] args) {
+        String sentence = "Hello, how are you?";
+        String modifiedSentence = sentence.replace(' ', '%');
+        System.out.println("Modified sentence: " + modifiedSentence);
+    }
+}
