b390fd421d3dafeafcd81410b64912e037e70bb7
samuel150a <samuelofficial184@gmail.com>
2025-04-23 01:08:26 +0530

    init commit

---
+++ Interface/src/com/btm/extenal/LaptopImple.java
@@ -0,0 +1,170 @@
+package com.btm.extenal;
+
+import com.btm.internal.*;
+
+public class LaptopImple implements Laptop, Showroom, Accessories, Workers, Salary
+{
+    @Override
+    public void Currency() {
+        System.out.println("running in the Absract method Currency after Implementation");
+    }
+
+    @Override
+    public void Pf() {
+        System.out.println("running in the Absract method Pf after Implementation");
+    }
+
+    @Override
+    public void Loan() {
+        System.out.println("running in the Absract method Loan after Implementation");
+    }
+
+    @Override
+    public void Spent() {
+        System.out.println("running in the Absract method Spent after Implementation");
+    }
+
+    @Override
+    public void Balance() {
+        System.out.println("running in the Absract method Balance after Implementation");
+    }
+
+    @Override
+    public void Socialwork() {
+        System.out.println("running in the Absract method Socialwork after Implementation");
+    }
+    @Override
+    public void donate() {
+        System.out.println("running in the Absract method donate after Implementation");
+    }
+
+    @Override
+    public void Age() {
+        System.out.println("running in the Absract method Age after Implementation");
+    }
+
+    @Override
+    public void Qualification() {
+        System.out.println("running in the Absract method Qualification after Implementation");
+    }
+    @Override
+    public void Experience() {
+        System.out.println("running in the Absract method Experience after Implementation");
+    }
+    @Override
+    public void talent() {
+        System.out.println("running in the Absract method talent after Implementation");
+    }
+
+    @Override
+    public void Skills() {
+        System.out.println("running in the Absract method Skills after Implementation");
+    }
+
+    @Override
+    public void Strength() {
+        System.out.println("running in the Absract method Strength after Implementation");
+    }
+
+    @Override
+    public void Weakness() {
+        System.out.println("running in the Absract method Weakness after Implementation");
+    }
+
+    @Override
+    public void Type() {
+        System.out.println("running in the Absract method Type after Implementation");
+    }
+    @Override
+    public void Discount() {
+        System.out.println("running in the Absract method Discount after Implementation");
+    }
+    @Override
+    public void Material(){
+        System.out.println("running in the Absract method  Material after Implementation");
+    }
+    @Override
+    public void Sizes() {
+        System.out.println("running in the Absract method  Sizes after Implementation");
+    }
+    @Override
+    public void Free() {
+        System.out.println("running in the Absract method Free after Implementation");
+    }
+    @Override
+    public void Warrenty(){
+        System.out.println("running in the Absract method  Warrenty after Implementation");
+    }
+
+    @Override
+    public void durability() {
+        System.out.println("running in the Absract method durability after Implementation");
+    }
+
+    @Override
+    public void Location() {
+        System.out.println("running in the Absract method  Location after Implementation");
+    }
+    @Override
+    public void Area() {
+        System.out.println("running in the Absract method Area after Implementation");
+    }
+
+    @Override
+    public void Street() {
+            System.out.println("running in the Absract method Street after Implementation");
+        }
+
+    @Override
+    public void Name() {
+            System.out.println("running in the Absract method Name after Implementation");
+        }
+
+    @Override
+    public void Rent() {
+            System.out.println("running in the Absract method Rent after Implementation");
+        }
+
+    @Override
+    public void Workers() {
+            System.out.println("running in the Absract method Workers after Implementation");
+        }
+
+    @Override
+    public void custumber() {
+            System.out.println("running in the Absract method custumber  after Implementation");
+        }
+    @Override
+    public void Color() {
+            System.out.println("running in the Absract method Color after Implementation");
+        }
+
+    @Override
+    public void Ram() {
+            System.out.println("running in the Absract method Ram after Implementation");
+        }
+
+    @Override
+    public void Company() {
+            System.out.println("running in the Absract method Company after Implementation");
+        }
+
+    @Override
+    public void Size() {
+            System.out.println("running in the Absract method Size after Implementation");
+        }
+
+    @Override
+    public void Screen() {
+            System.out.println("running in the Absract method Screen after Implementation");
+        }
+
+    @Override
+    public void weight() {
+            System.out.println("running in the Absract method weight after Implementation");
+        }
+    @Override
+    public void Keylight() {
+            System.out.println("running in the Absract method Keylight after Implementation");
+        }
+}
