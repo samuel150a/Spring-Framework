6aa9400affab2fe833b32e5c942aa795616dc8d8
samuel150a <samuelofficial184@gmail.com>
2025-04-22 09:00:02 +0530

    init commit

---
+++ Abstract/src/com/abstrat/runner/Runnings.java
@@ -0,0 +1,30 @@
+package com.abstrat.runner;
+
+import com.abstrat.internal.*;
+
+
+public class Runnings {
+    public static void main(String[] args) {
+        HomeSpray homeSpray = new HomeSpray();
+        homeSpray.incredent();
+
+        Color color = new Color();
+        color.Eclairs();
+
+        Amul amul = new Amul();
+        amul.Brand();
+        amul.Price();
+
+        AbstractIceCream Amul1 = new Amul();
+        Amul1.Brand();
+
+        AbstractHomework a1 = new HomeworkResult();
+        AbstractHomeworkProgress a2 = new HomeworkResult();
+        HomeworkResult a3 = new HomeworkResult();
+        a1.English();
+        a2.English();
+        a3.English();
+
+
+    }
+}
