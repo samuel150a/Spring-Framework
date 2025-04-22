75b465bbd96ce52fe5c57e16fdb20b799cc9175a
samuel150a <samuelofficial184@gmail.com>
2025-04-22 08:58:08 +0530

    init commit

---
+++ Abstract/src/com/abstrat/internal/AbstractHomeworkProgress.java
@@ -0,0 +1,12 @@
+package com.abstrat.internal;
+
+public abstract class  AbstractHomeworkProgress extends AbstractHomework
+{
+  @Override
+    public   void Maths()
+    {
+        System.out.println("Running in the Abstract method maths after implementation");
+    }
+
+}
+
