b771f4a63cb17e8cca890dbec6248c4c67b60ec3
samuel150a <samuelofficial184@gmail.com>
2025-04-23 01:08:57 +0530

    init commit

---
+++ Interface/src/com/btm/internal/Mobile.java
@@ -0,0 +1,14 @@
+package com.btm.internal;
+
+public interface Mobile {
+    void Recharge();
+    void Battrey();
+    void Network();
+    void Simcard();
+    void Charger();
+    void Memory();
+    void Data();
+    void Poratable();
+    void Internet();
+    void Earphone();
+}
