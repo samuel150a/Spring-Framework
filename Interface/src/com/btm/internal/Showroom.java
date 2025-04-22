8fd11ef1ea4c9bc0c1b98cf3244be932c2db0421
samuel150a <samuelofficial184@gmail.com>
2025-04-23 01:11:48 +0530

    init commit

---
+++ Interface/src/com/btm/internal/Showroom.java
@@ -0,0 +1,11 @@
+package com.btm.internal;
+
+public interface Showroom extends  Laptop{
+    void Location();
+    void Area();
+    void Street();
+    void Name();
+    void Rent();
+    void Workers();
+    void custumber();
+}
