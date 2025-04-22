650f8ac9af5747e08cff6421f68be9eccae81bb8
samuel150a <samuelofficial184@gmail.com>
2025-04-22 08:57:55 +0530

    init commit

---
+++ Abstract/src/com/abstrat/internal/AbstractChocolate.java
@@ -0,0 +1,7 @@
+
+/* 2. Declare an abstract class with abstract method and extend it by a subclass. Create copy of subclass*/
+package com.abstrat.internal;
+
+public abstract class AbstractChocolate {
+    public  abstract void  Eclairs();
+}
