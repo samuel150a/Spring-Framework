f2442c9f82bdfe4cdd978923a353825683107840
samuel150a <samuelofficial184@gmail.com>
2025-04-23 01:12:10 +0530

    init commit

---
+++ Interface/src/com/btm/internal/Workers.java
@@ -0,0 +1,13 @@
+package com.btm.internal;
+
+public interface Workers extends  Accessories{
+
+    void Age();
+    void Qualification();
+    void Experience();
+    void talent();
+    void Skills();
+    void Strength();
+    void Weakness();
+
+}
