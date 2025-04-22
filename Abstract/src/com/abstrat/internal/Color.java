db8bb3ceca02583d8ec8ad185786024bcb33e19b
samuel150a <samuelofficial184@gmail.com>
2025-04-22 08:59:23 +0530

    init commit

---
+++ Abstract/src/com/abstrat/internal/Color.java
@@ -0,0 +1,10 @@
+package com.abstrat.internal;
+
+public  class Color extends AbstractChocolate {
+    @Override
+    public   void  Eclairs(){
+        System.out.println("Runnning in the Eclairs methods After implementation");
+
+    }
+
+}
