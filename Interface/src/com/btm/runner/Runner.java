023a80d2ebbc7fdacccf3768807369ed73833535
samuel150a <samuelofficial184@gmail.com>
2025-04-23 01:10:32 +0530

    init commit

---
+++ Interface/src/com/btm/runner/Runner.java
@@ -0,0 +1,14 @@
+package com.btm.runner;
+
+import com.btm.extenal.Kaveri;
+import com.btm.internal.Hospital;
+
+public class Runner {
+    public static void main(String[] args) {
+        Kaveri kaveri=new Kaveri();
+        Hospital hospital=new Kaveri();
+        kaveri.clean();
+        hospital.clean();
+
+        }
+}
