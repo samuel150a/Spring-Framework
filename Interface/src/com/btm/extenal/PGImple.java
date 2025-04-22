dc52a19a1bdca22676fd2b24b6e8507d5bdec0ca
samuel150a <samuelofficial184@gmail.com>
2025-04-23 01:09:57 +0530

    init commit

---
+++ Interface/src/com/btm/extenal/PGImple.java
@@ -0,0 +1,157 @@
+package com.btm.extenal;
+
+import com.btm.internal.*;
+
+public class PGImple implements PG, Wifi, Person, Lift, Rooms {
+
+    @Override
+    public void Ac() {
+        System.out.println("Running in the Ac abstract method Ac after implementation");
+
+    }
+
+    @Override
+    public void members() {
+        System.out.println("Running in the Ac abstract method members after implementation");
+
+    }
+
+    @Override
+    public void Beds() {
+        System.out.println("Running in the Ac abstract method Beds after implementation");
+
+    }
+
+    @Override
+    public void Tv() {
+        System.out.println("Running in the Ac abstract method Tv(after implementation");
+
+    }
+
+    @Override
+    public void Table() {
+        System.out.println("Running in the Ac abstract method Table after implementation");
+
+    }
+
+    @Override
+    public void Load() {
+        System.out.println("Running in the Ac abstract method Load after implementation");
+
+    }
+
+    @Override
+    public void Power() {
+        System.out.println("Running in the Ac abstract method  Power after implementation");
+
+    }
+
+    @Override
+    public void Flexibility() {
+        System.out.println("Running in the Ac abstract method Flexibility after implementation");
+
+    }
+
+    @Override
+    public void Floors()
+        {
+            System.out.println("Running in the Ac abstract method Floors after implementation");
+
+        }
+
+    @Override
+    public void Costiss() {
+        System.out.println("Running in the Ac abstract method Costiss after implementation");
+
+    }
+
+    @Override
+    public void Numbers() {
+        System.out.println("Running in the Ac abstract method Numbers after implementation");
+
+    }
+
+    @Override
+    public void Age() {
+        System.out.println("Running in the Ac abstract method Age after implementation");
+
+    }
+
+    @Override
+    public void Identity () {
+        System.out.println("Running in the Ac abstract method Identity after implementation");
+
+    }
+
+    @Override
+    public void income() {
+        System.out.println("Running in the Ac abstract method income after implementation");
+
+    }
+
+    @Override
+    public void  Address(){
+        System.out.println("Running in the Ac abstract method Address after implementation");
+
+    }
+
+    @Override
+    public void Range() {
+        System.out.println("Running in the Ac abstract method Range after implementation");
+
+    }
+
+    @Override
+    public void Speed() {
+        System.out.println("Running in the Ac abstract method Speed after implementation");
+
+    }
+
+    @Override
+    public void Price() {
+        System.out.println("Running in the Ac abstract method Price after implementation");
+
+    }
+
+    @Override
+    public void Duration() {
+        System.out.println("Running in the Ac abstract method Duration after implementation");
+
+    }
+
+    @Override
+    public void  Capacity(){
+        System.out.println("Running in the Ac abstract method Capacity after implementation");
+
+    }
+
+    @Override
+    public void Rent() {
+        System.out.println("Running in the Ac abstract method Rent after implementation");
+
+    }
+
+    @Override
+    public void Days() {
+        System.out.println("Running in the Ac abstract method Days after implementation");
+
+    }
+
+    @Override
+    public void Maintenance() {
+        System.out.println("Running in the Ac abstract method Maintenance after implementation");
+
+    }
+
+    @Override
+    public void Food() {
+        System.out.println("Running in the Ac abstract method  Food after implementation");
+
+    }
+
+    @Override
+    public void Facility(){
+        System.out.println("Running in the Ac abstract method Facility after implementation");
+
+    }
+}
