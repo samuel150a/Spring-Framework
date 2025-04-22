693590c757b81fe64f05ee7c09bbbb379d6053c9
samuel150a <samuelofficial184@gmail.com>
2025-04-22 08:59:47 +0530

    init commit

---
+++ Abstract/src/com/abstrat/internal/HomeworkResult.java
@@ -0,0 +1,11 @@
+package com.abstrat.internal;
+
+public class HomeworkResult extends AbstractHomeworkProgress
+{
+    @Override
+    public  void English(){
+        System.out.println("Running in the Abstract method English after implementation");
+
+
+    }
+}
