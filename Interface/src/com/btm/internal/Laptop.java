6dc5b071c3aee3600c4d35839cba80ec642adb0d
samuel150a <samuelofficial184@gmail.com>
2025-04-23 01:08:13 +0530

    init commit

---
+++ Interface/src/com/btm/internal/Laptop.java
@@ -0,0 +1,13 @@
+package com.btm.internal;
+
+public interface Laptop {
+    void Color();
+    void Ram();
+    void Company();
+    void Size() ;
+    void Screen();
+    void weight();
+    void Keylight();
+
+}
+
