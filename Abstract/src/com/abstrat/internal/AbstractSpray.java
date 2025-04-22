0f47c273a166f817d9dd55ea5fd5e5177892f19f
samuel150a <samuelofficial184@gmail.com>
2025-04-22 08:58:53 +0530

    init commit

---
+++ Abstract/src/com/abstrat/internal/AbstractSpray.java
@@ -0,0 +1,13 @@
+
+/*Declare an abstract class with no abstract methods and extend it by a subclass. Create copy of subclass*/
+
+package com.abstrat.internal;
+
+public  abstract class AbstractSpray {
+
+        public void incredent()
+        {
+            System.out.println("no Abstract method is running");
+        }
+
+}
