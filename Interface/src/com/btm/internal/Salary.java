e3680a1bbcc5f30a8a8f1a29fe663786db4c0341
samuel150a <samuelofficial184@gmail.com>
2025-04-23 01:11:01 +0530

    init commit

---
+++ Interface/src/com/btm/internal/Salary.java
@@ -0,0 +1,13 @@
+package com.btm.internal;
+
+public interface Salary extends Workers
+{
+    void Currency();
+    void Pf();
+    void Loan();
+    void Spent();
+    void Balance();
+    void Socialwork();
+    void donate();
+
+}
