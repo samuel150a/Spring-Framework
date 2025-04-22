b89dcda03333df0bd414e6ab9e92e2bf43ba4585
samuel150a <samuelofficial184@gmail.com>
2025-04-23 01:09:06 +0530

    init commit

---
+++ Interface/src/com/btm/extenal/MobileImple.java
@@ -0,0 +1,280 @@
+package com.btm.extenal;
+
+import com.btm.internal.*;
+
+public class MobileImple implements Mobile, MobileShop, Blutooth, Call, ChargerSocket
+{
+
+
+    @Override
+    public void PlugInCharger() {
+        System.out.println("Running in the Abstract PlugInCharger method After implementation");
+
+    }
+
+    @Override
+    public void UnplugCharger() {
+        System.out.println("Running in the Abstract method UnplugCharger After implementation");
+
+    }
+
+    @Override
+    public void Compatibility() {
+        System.out.println("Running in the Abstract method After implementation");
+
+    }
+
+    @Override
+    public void TestChargingPort() {
+        System.out.println("Running in the Abstract method Compatibility After implementation");
+
+    }
+
+    @Override
+    public void ReplaceChargingSocket() {
+        System.out.println("Running in the Abstract method ReplaceChargingSocket After implementation");
+
+    }
+
+    @Override
+    public void CleanChargingPort() {
+        System.out.println("Running in the Abstract method After implementation");
+
+    }
+
+    @Override
+    public void DetectCharging() {
+        System.out.println("Running in the Abstract method CleanChargingPort After implementation");
+
+    }
+
+    @Override
+    public void ShowChargingStatus() {
+        System.out.println("Running in the Abstract method  ShowChargingStatus After implementation");
+
+    }
+
+    @Override
+    public void EnableFastCharging(){
+        System.out.println("Running in the Abstract method EnableFastCharging After implementation");
+
+    }
+
+    @Override
+    public void MakeCall() {
+        System.out.println("Running in the Abstract method MakeCall After implementation");
+
+    }
+
+    @Override
+    public void ReceiveCall() {
+        System.out.println("Running in the Abstract method ReceiveCall After implementation");
+
+    }
+
+    @Override
+    public void EndCall() {
+        System.out.println("Running in the Abstract method EndCall After implementation");
+
+    }
+
+    @Override
+    public void HoldCall() {
+        System.out.println("Running in the Abstract method HoldCall After implementation");
+
+    }
+
+    @Override
+    public void MuteCall() {
+        System.out.println("Running in the Abstract method MuteCall After implementation");
+
+    }
+
+    @Override
+    public void UnmuteCall() {
+        System.out.println("Running in the Abstract method UnmuteCall After implementation");
+
+    }
+
+    @Override
+    public void RecordCall() {
+        System.out.println("Running in the Abstract method RecordCall After implementation");
+
+    }
+
+    @Override
+    public void BlockNumber() {
+        System.out.println("Running in the Abstract method BlockNumber After implementation");
+
+    }
+
+    @Override
+    public void ViewCallHistory(){
+        System.out.println("Running in the Abstract method ViewCallHistory After implementation");
+
+    }
+    @Override
+    public void EnableBluetooth() {
+        System.out.println("Running in the Abstract method EnableBluetooth After implementation");
+
+    }
+    @Override
+    public void DisableBluetooth() {
+        System.out.println("Running in the Abstract method DisableBluetooth After implementation");
+
+    }
+
+    @Override
+    public void PairBluetoothDevice() {
+        System.out.println("Running in the Abstract method PairBluetoothDevice After implementation");
+
+    }
+
+    @Override
+    public void UnpairBluetoothDevice() {
+        System.out.println("Running in the Abstract method UnpairBluetoothDevice After implementation");
+
+    }
+
+    @Override
+    public void SearchBluetoothDevices() {
+        System.out.println("Running in the Abstract method SearchBluetoothDevices After implementation");
+
+    }
+
+    @Override
+    public void ShowPairedDevices() {
+        System.out.println("Running in the Abstract method ShowPairedDevices After implementation");
+
+    }
+
+    @Override
+    public void SendFileViaBluetooth() {
+        System.out.println("Running in the Abstract method SendFileViaBluetooth After implementation");
+
+    }
+    @Override
+    public void ReceiveFileViaBluetooth() {
+        System.out.println("Running in the Abstract method ReceiveFileViaBluetooth After implementation");
+
+    }
+
+    @Override
+    public void BluetoothStatus() {
+        System.out.println("Running in the Abstract method BluetoothStatus After implementation");
+
+    }
+
+    @Override
+    public void AvailableMobiles() {
+        System.out.println("Running in the Abstract method AvailableMobiles After implementation");
+
+    }
+    @Override
+    public void MobilePrice() {
+        System.out.println("Running in the Abstract method  MobilePrice  After implementation");
+
+    }
+
+    @Override
+    public void MobileStock() {
+        System.out.println("Running in the Abstract method MobileStock After implementation");
+
+    }
+
+    @Override
+    public void SellMobile() {
+        System.out.println("Running in the Abstract method SellMobile  After implementation");
+
+    }
+
+    @Override
+    public void WarrantyDetails() {
+        System.out.println("Running in the Abstract method WarrantyDetails After implementation");
+
+    }
+
+    @Override
+    public void Discounts() {
+        System.out.println("Running in the Abstract method Discounts After implementation");
+
+    }
+
+    @Override
+    public void SimCard() {
+        System.out.println("Running in the Abstract method SimCard After implementation");
+
+    }
+
+    @Override
+    public void RBattery() {
+        System.out.println("Running in the Abstract method RBattery After implementation");
+
+    }
+
+    @Override
+    public void Connectivity() {
+        System.out.println("Running in the Abstract method Connectivity After implementation");
+
+    }
+
+    @Override
+    public void Recharge() {
+        System.out.println("Running in the Abstract method Recharge After implementation");
+
+    }
+
+    @Override
+    public void Battrey() {
+        System.out.println("Running in the Abstract method Battrey After implementation");
+
+    }
+
+    @Override
+    public void Network() {
+        System.out.println("Running in the Abstract method Network After implementation");
+
+    }
+
+    @Override
+    public void Simcard() {
+        System.out.println("Running in the Abstract method Simcard After implementation");
+
+    }
+
+    @Override
+    public void Charger() {
+        System.out.println("Running in the Abstract method Charger After implementation");
+
+    }
+
+    @Override
+    public void Memory() {
+        System.out.println("Running in the Abstract method Memory After implementation");
+
+    }
+
+    @Override
+    public void Data() {
+        System.out.println("Running in the Abstract method Data After implementation");
+
+    }
+
+    @Override
+    public void Poratable() {
+        System.out.println("Running in the Abstract method Poratable After implementation");
+
+    }
+
+    @Override
+    public void Internet() {
+        System.out.println("Running in the Abstract method Internet After implementation");
+
+    }
+
+    @Override
+    public void Earphone() {
+        System.out.println("Running in the Abstract method Earphone After implementation");
+
+    }
+}
