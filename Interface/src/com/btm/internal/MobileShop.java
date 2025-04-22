1d7b681499d0316b3d88df7fd49e98e45da79301
samuel150a <samuelofficial184@gmail.com>
2025-04-23 01:09:18 +0530

    init commit

---
+++ Interface/src/com/btm/internal/MobileShop.java
@@ -0,0 +1,23 @@
+package com.btm.internal;
+
+public interface MobileShop extends Mobile {
+
+
+    void AvailableMobiles();
+
+    void MobilePrice();
+
+    void MobileStock();
+
+    void SellMobile();
+
+    void WarrantyDetails();
+
+    void Discounts();
+
+    void SimCard();
+
+    void RBattery();
+
+    void Connectivity();
+}
