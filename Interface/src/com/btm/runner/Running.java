2ca4a4028cde42cc8218923bc28493c9c438e65c
samuel150a <samuelofficial184@gmail.com>
2025-04-22 22:23:27 +0530

    init commit

---
+++ Interface/src/com/btm/runner/Running.java
@@ -0,0 +1,30 @@
+package com.btm.runner;
+
+import com.btm.extenal.PeterHall;
+import com.btm.internal.CCtv;
+
+public class Running {
+    public static void main(String[] args) {
+        PeterHall peterHall= new PeterHall();
+        CCtv p1=new PeterHall();
+
+        peterHall.Alert();
+        peterHall.Foottage();
+        peterHall.Silence();
+        peterHall.Uniform();
+        peterHall.Verification();
+
+        p1.Alert();
+        p1.Foottage();
+        p1.Silence();
+        p1.Uniform();
+        p1.Verification();
+
+
+
+
+
+
+
+    }
+}
