8f8b7a7bcc060ee296d64e7d427c7118aca647b8
samuel150a <samuelofficial184@gmail.com>
2025-04-23 01:07:44 +0530

    init commit

---
+++ Interface/src/com/btm/internal/Accessories.java
@@ -0,0 +1,17 @@
+package com.btm.internal;
+
+public interface Accessories extends Showroom {
+    void Type();
+
+    void Discount();
+
+    void Material();
+
+    void Sizes();
+
+    void Free();
+
+    void Warrenty();
+
+    void durability();
+}
