8fa86346669eccb175de7798154a484f037ff2cb
samuel150a <samuelofficial184@gmail.com>
2025-04-22 09:00:21 +0530

    init commit

---
+++ Abstract/src/com/abstrat/internal/Pant.java
@@ -0,0 +1,13 @@
+package com.abstrat.internal;
+
+
+
+public class Pant extends Material {
+
+    int button;
+    public Material(String cloth, int button)
+    {
+        super(cloth);
+        this.button=button;
+    }
+}
