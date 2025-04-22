146913a82dc63b48851e2b13c50289ec0cc9e4e2
samuel150a <samuelofficial184@gmail.com>
2025-04-22 08:59:34 +0530

    init commit

---
+++ Abstract/src/com/abstrat/internal/HomeSpray.java
@@ -0,0 +1,9 @@
+package com.abstrat.internal;
+
+public class HomeSpray extends AbstractSpray
+{
+    public void incredent()
+    {
+        System.out.println("no Abstract method is running after implementation");
+    }
+}
