401eb666b7fe7f00543c2f33f86195e6b8387f6f
samuel150a <samuelofficial184@gmail.com>
2025-04-22 09:00:35 +0530

    init commit

---
+++ Abstract/src/com/abstrat/internal/Amul.java
@@ -0,0 +1,12 @@
+package com.abstrat.internal;
+
+public class Amul extends AbstractIceCream
+{
+    @Override
+    public  void Price(){
+        System.out.println("Running in Abstract price method after implementation");
+    }
+
+
+}
+
