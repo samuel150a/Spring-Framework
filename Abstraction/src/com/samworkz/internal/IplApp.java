ca9cd702d537c9951d3f1a7a34c9bf911c19916a
samuel150a <samuelofficial184@gmail.com>
2025-04-24 20:20:37 +0530

    init commit

--- Abstraction/src/com/samworkz/internal/IplApp.java
+++ Abstraction/src/com/samworkz/internal/IplApp.java
@@ -4,13 +4,13 @@ public class IplApp {
     private JDBC jdbc;
 
     public IplApp(JDBC jdbc) {
+
         this.jdbc = jdbc;
     }
 
     public void Details() {
         if (jdbc != null) {
-
-            System.out.println("running in the details");
+            this.jdbc.Save();
 
         }
         else {
