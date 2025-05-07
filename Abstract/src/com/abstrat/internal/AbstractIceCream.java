b81234f2418b96a4b0d698f794c6bb8f5976b9de
samuel150a <samuelofficial184@gmail.com>
2025-05-07 23:32:31 +0530

    init commit

--- Abstract/src/com/abstrat/internal/AbstractIceCream.java
+++ Abstract/src/com/abstrat/internal/AbstractIceCream.java
@@ -1,6 +1,7 @@
 /* 3. Declare an abstract class with one abstract method and one concrete method and extend it by a subclass. Create copy of subclass */
 package com.abstrat.internal;
 
+
 public abstract class AbstractIceCream {
     public abstract void Price();
     public void Brand()
