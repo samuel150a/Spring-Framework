cf6ec419716f538e2b18fca2d1fb749806e41f5b
samuel150a <samuelofficial184@gmail.com>
2025-05-07 23:32:06 +0530

    init commit

---
+++ Webstart/src/com/xworkz/internal/Samuel.java
@@ -0,0 +1,14 @@
+package com.xworkz.internal;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class Samuel extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+}
