5ce4ed1399240d4e25d80fd73ff47f0fe8e2d748
samuel150a <samuelofficial184@gmail.com>
2025-05-16 02:40:50 +0530

    init commit

--- ServletJsp-Projects/src/com/xworkz/DonationServlet.java
+++ ServletJsp-Projects/src/com/xworkz/DonationServlet.java
@@ -2,33 +2,50 @@ package com.xworkz;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebServlet(urlPatterns="/Do")
-public class DonationServlet extends GenericServlet {
-    @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        String name = servletRequest.getParameter("name");
+public class DonationServlet extends HttpServlet {
+
+
+    public DonationServlet()
+    {
+        System.out.println("Running in the constructor called DonationServlet");
+    }
+   // @Override
+    //public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {//
+        @Override
+           protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+        {
+
+    System.out.println("Running in the Post method");
+    String name = req.getParameter("name");
 
         System.out.println("Received name: " + name);
 
-        String email = servletRequest.getParameter("email");
+        String email = req.getParameter("email");
 
         System.out.println("Received email: " + email);
 
 
-        String amount = servletRequest.getParameter("amount");
+        String amount = req.getParameter("amount");
 
         System.out.println("Received amount: " + amount);
 
-        String org = servletRequest.getParameter("org");
+        String org = req.getParameter("org");
 
         System.out.println("Received org: " + org);
 
         System.out.println("using request dispatcher to forward the req and res to another jsp/servlet");
 
-        RequestDispatcher requestDispatcher= servletRequest.getRequestDispatcher("SuccessDonation.jsp");
-
-        requestDispatcher.forward(servletRequest,servletResponse);
+        RequestDispatcher requestDispatcher= req.getRequestDispatcher("SuccessDonation.jsp");
+            req.setAttribute("name",name);
+            req.setAttribute("email",email);
+            req.setAttribute("amount",amount);
+            req.setAttribute("org",org);
+        requestDispatcher.forward(req,resp);
 
 
     }
