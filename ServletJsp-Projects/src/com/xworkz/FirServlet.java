577e44887bd667332f382dd956262192e64e58ce
samuel150a <samuelofficial184@gmail.com>
2025-05-16 02:39:57 +0530

    init commit

--- ServletJsp-Projects/src/com/xworkz/FirServlet.java
+++ ServletJsp-Projects/src/com/xworkz/FirServlet.java
@@ -11,25 +11,45 @@ import java.io.IOException;
 
 import javax.servlet.*;
         import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebServlet(urlPatterns="/Reg")
-public class FirServlet extends GenericServlet {
+
+
+public class FirServlet extends HttpServlet
+
+{
+    public FirServlet(){
+        System.out.println("Running in the FirServlet constructor");
+    }
     @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        String pname=servletRequest.getParameter("pname");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException
+    {
+        System.out.println("Post method in furniture Servlet");
+
+        String pname=req.getParameter("pname");
         System.out.println(pname);
 
-        String name=servletRequest.getParameter("name");
+        String name=req.getParameter("name");
         System.out.println(name);
-        String Complaint=servletRequest.getParameter("sub");
+        String Complaint=req.getParameter("sub");
         System.out.println(Complaint);
-        String city=servletRequest.getParameter("city");
+        String city=req.getParameter("city");
         System.out.println(city);
-        String Aadhar=servletRequest.getParameter("proof");
+        String Aadhar=req.getParameter("proof");
         System.out.println(Aadhar);
 
 
-        RequestDispatcher requestDispatcher=servletRequest.getRequestDispatcher("SuccessFIR.jsp");
-        requestDispatcher.forward(servletRequest,servletResponse);
+        RequestDispatcher requestDispatcher=req.getRequestDispatcher("SuccessFIR.jsp");
+        req.setAttribute("pname",pname);
+        req.setAttribute("name",name);
+        req.setAttribute("sub",Complaint);
+        req.setAttribute("city",city);
+        req.setAttribute("proof",Aadhar);
+
+
+        requestDispatcher.forward(req,resp);
     }
 }
