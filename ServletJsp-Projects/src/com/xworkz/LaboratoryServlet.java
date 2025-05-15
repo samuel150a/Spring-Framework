b3015bb955ffeb3bb5c35078ecd289bcd935209a
samuel150a <samuelofficial184@gmail.com>
2025-05-16 02:40:13 +0530

    init commit

--- ServletJsp-Projects/src/com/xworkz/LaboratoryServlet.java
+++ ServletJsp-Projects/src/com/xworkz/LaboratoryServlet.java
@@ -2,15 +2,30 @@ package com.xworkz;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebServlet(urlPatterns = "/Lab")
-public class LaboratoryServlet extends GenericServlet {
-    @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        String pname=servletRequest.getParameter("name");
-        String age=servletRequest.getParameter("age");
-        String bg=servletRequest.getParameter("g");
-        String bp=servletRequest.getParameter("p");
+public class LaboratoryServlet extends HttpServlet {
+
+    public LaboratoryServlet()
+    {
+        System.out.println("Running in the LabaratoryServlet Constructor");
+    }
+
+
+
+    //@Override
+    //public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+System.out.println("Running in the post method");
+
+        String pname=req.getParameter("name");
+        String age=req.getParameter("age");
+        String bg=req.getParameter("g");
+        String bp=req.getParameter("p");
 
 
         System.out.println(pname);
@@ -19,8 +34,14 @@ public class LaboratoryServlet extends GenericServlet {
         System.out.println(bp);
 
 
-        RequestDispatcher requestDispatcher=servletRequest.getRequestDispatcher("SuccessLaboratory.jsp");
-        requestDispatcher.forward(servletRequest,servletResponse);
+        RequestDispatcher requestDispatcher=req.getRequestDispatcher("SuccessLaboratory.jsp");
+
+        req.setAttribute("name",pname);
+        req.setAttribute("age",age);
+        req.setAttribute("g",bg);
+        req.setAttribute("p",bp);
+
+        requestDispatcher.forward(req,resp);
 
     }
 }
