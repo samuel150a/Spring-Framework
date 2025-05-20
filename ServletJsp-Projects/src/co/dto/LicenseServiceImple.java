db1c78319837d0e27b25e064180ef6274becd47f
samuel150a <samuelofficial184@gmail.com>
2025-05-20 12:22:06 +0530

    init commit

---
+++ ServletJsp-Projects/src/co/dto/LicenseServiceImple.java
@@ -0,0 +1,16 @@
+package co.dto;
+
+import co.service.LicenseService;
+
+public class LicenseServiceImple implements LicenseService {
+    public LicenseServiceImple()
+    {
+        System.out.println("running in the constructor of LSerImple");
+    }
+
+    @Override
+    public boolean save(LicenseDto dto) {
+        System.out.println("save method in LicenseServiceImple class");
+        return true;
+    }
+}
