a6081e87f94ff58280bf712b0a11b0902d939dd9
samuel150a <samuelofficial184@gmail.com>
2025-05-20 12:00:04 +0530

    init commit

--- ServletJsp-Projects/src/com/xworkz/FirServlet.java
+++ ServletJsp-Projects/src/com/xworkz/FirServlet.java
@@ -1,6 +1,8 @@
 package com.xworkz;
 
 import co.dto.FIRDto;
+import co.dto.FirServiceImpl;
+import co.service.FirService;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
@@ -14,31 +16,30 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns="/Reg")
 
 
-public class FirServlet extends HttpServlet
-
-{
-    public FirServlet(){
+public class FirServlet extends HttpServlet {
+    public FirServlet() {
         System.out.println("Running in the FirServlet constructor");
     }
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException
-    {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Post method in furniture Servlet");
 
-        String pname=req.getParameter("pname");
+        String pname = req.getParameter("pname");
         System.out.println(pname);
 
-        String name=req.getParameter("name");
+        String name = req.getParameter("name");
         System.out.println(name);
 
-        String complaint=req.getParameter("complaint");
+        String complaint = req.getParameter("complaint");
         System.out.println(complaint);
-        String city=req.getParameter("city");
+        String city = req.getParameter("city");
         System.out.println(city);
-        String aadhar=req.getParameter("aadhar");
+        String aadhar = req.getParameter("aadhar");
         System.out.println(aadhar);
 
-        FIRDto dto=new FIRDto();
+        FIRDto dto = new FIRDto();
+        FirService service = new FirServiceImpl();
 
         dto.setpname(pname);
         dto.setname(name);
@@ -47,8 +48,21 @@ public class FirServlet extends HttpServlet
         dto.setaadhar(aadhar);
 
 
-        req.setAttribute("dto",dto);
-        RequestDispatcher requestDispatcher=req.getRequestDispatcher("SuccessFIR.jsp");
-        requestDispatcher.forward(req,resp);
+        boolean saved = service.save(dto);
+
+        if (saved) {
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("SuccessFIR.jsp");
+            req.setAttribute("dto", dto);
+
+            requestDispatcher.forward(req, resp);
+        }
+
+        else {
+
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("FIR.jsp");
+            req.setAttribute("dto", dto);
+
+            requestDispatcher.forward(req, resp);
+        }
     }
 }
