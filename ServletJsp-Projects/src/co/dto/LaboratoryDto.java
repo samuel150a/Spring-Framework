818e50ab267f5cc188470c962692bcbabc6b0acb
samuel150a <samuelofficial184@gmail.com>
2025-05-20 18:32:10 +0530

    init commit

---
+++ ServletJsp-Projects/src/co/dto/LaboratoryDto.java
@@ -0,0 +1,50 @@
+package co.dto;
+
+public class LaboratoryDto {
+
+    private String name;
+    private String age;
+    private String bloodgroup;
+    private String bloodpressure;
+
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public void setage(String age) {
+        this.age = age;
+    }
+
+
+    public void setbloodgroup(String bloodgroup) {
+        this.bloodgroup = bloodgroup;
+    }
+
+    public void setbloodpressure(String bloodpressure) {
+        this.bloodpressure = bloodpressure;
+    }
+
+
+    public String getname()
+
+    {
+        return name;
+    }
+
+    public String getage()
+    {
+        return age;
+    }
+
+    public String getbloodgroup()
+    {
+        return bloodgroup;
+    }
+
+    public String getbloodpressure()
+    {
+        return bloodpressure;
+    }
+
+}
