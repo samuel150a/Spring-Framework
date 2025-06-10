31237e22b4a52242838579e317f91acbb2679e08
samuel150a <samuelofficial184@gmail.com>
2025-06-10 21:23:13 +0530

    init commit

---
+++ Exceptions/src/com/xworkz/InterviewException.java
@@ -0,0 +1,24 @@
+package com.xworkz;
+
+
+
+public class InterviewException extends Exception {
+    public  InterviewException(String message)
+    {
+        super(message);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
