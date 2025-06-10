0691c427c39f2a2b8e83c515a02fe8cdaf5fd9e9
samuel150a <samuelofficial184@gmail.com>
2025-06-10 21:21:29 +0530

    init commit

--- ServletJsp-Projects/src/co/dto/FirServiceImpl.java
+++ ServletJsp-Projects/src/co/dto/FirServiceImpl.java
@@ -1,17 +1,19 @@
 package co.dto;
 
+import co.repository.FirRepository;
+import co.repository.FirRepositoryImpl;
 import co.service.FirService;
 
 public class FirServiceImpl  implements FirService {
-    public FirServiceImpl()
-    {
+    public FirServiceImpl() {
 
         System.out.println("Running in the implements constructor");
     }
 
     @Override
-    public boolean save(FIRDto dto) {
+    public boolean save(FIRDto firDto) {
         System.out.println("save method in Fur impl Class");
-        return true;
+        FirRepository firRepository = new FirRepositoryImpl();
+        return firRepository.persist(firDto);
     }
 }
