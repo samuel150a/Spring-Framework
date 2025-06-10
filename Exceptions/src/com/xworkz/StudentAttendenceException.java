5295fa767755e1e806a82dad9324b3708ed12d4f
samuel150a <samuelofficial184@gmail.com>
2025-06-10 21:24:03 +0530

    init commit

---
+++ Exceptions/src/com/xworkz/StudentAttendenceException.java
@@ -0,0 +1,22 @@
+package com.xworkz;
+
+public class StudentAttendenceException extends Exception {
+    public  StudentAttendenceException(String message)
+    {
+        super(message);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
