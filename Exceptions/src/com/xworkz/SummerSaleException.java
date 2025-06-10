b46f68a4104c01dc305b07a1e224abff1f0189b5
samuel150a <samuelofficial184@gmail.com>
2025-06-10 21:24:23 +0530

    init commit

---
+++ Exceptions/src/com/xworkz/SummerSaleException.java
@@ -0,0 +1,22 @@
+package com.xworkz;
+
+public class SummerSaleException extends Exception {
+    public  SummerSaleException(String message)
+    {
+        super(message);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
