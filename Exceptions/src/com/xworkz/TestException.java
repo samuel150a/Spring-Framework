ed31f550d8db1bfab862814b6b15a6767f285b09
samuel150a <samuelofficial184@gmail.com>
2025-06-10 21:25:07 +0530

    init commit

---
+++ Exceptions/src/com/xworkz/TestException.java
@@ -0,0 +1,24 @@
+package com.xworkz;
+
+
+
+public class TestException extends Exception {
+    public  TestException(String message)
+    {
+        super(message);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
