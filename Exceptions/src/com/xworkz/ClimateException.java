bbbe6c2b60701418ed132c66b72dfb0f29ad04d8
samuel150a <samuelofficial184@gmail.com>
2025-06-10 21:19:29 +0530

    init commit

---
+++ Exceptions/src/com/xworkz/ClimateException.java
@@ -0,0 +1,22 @@
+package com.xworkz;
+
+public class ClimateException extends Exception {
+    public  ClimateException(String message)
+    {
+        super(message);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
