9205c195381489885f10fc43a733643292662481
samuel150a <samuelofficial184@gmail.com>
2025-06-10 21:22:49 +0530

    init commit

---
+++ Exceptions/src/com/xworkz/KarnatakaBandhExeption.java
@@ -0,0 +1,25 @@
+package com.xworkz;
+
+
+
+
+public class KarnatakaBandhExeption extends Exception {
+    public  KarnatakaBandhExeption(String message)
+    {
+        super(message);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
