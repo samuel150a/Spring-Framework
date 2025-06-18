f46d77b1bac4d2eb9ad5564cb0e60f6ccb8e0fd7
samuel150a <samuelofficial184@gmail.com>
2025-06-18 12:25:56 +0530

    FindById

---
+++ Piano-FindById-Project/src/com/xworkz/piano/constant/DBProperties.java
@@ -0,0 +1,26 @@
+package com.xworkz.piano.constant;
+
+
+
+public enum DBProperties {
+
+    URL("jdbc:mysql://localhost:3306/xworkzz"),
+    USER_NAME("root"),
+    SECRET("Xworkz@123s"),
+    VERSION_8;
+
+    private String prop;
+
+    private  DBProperties(String prop)
+    {
+        this.prop=prop;
+    }
+    private DBProperties()
+    {
+
+    }
+
+    public String getProp() {
+        return prop;
+    }
+}
