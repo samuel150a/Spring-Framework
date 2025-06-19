0751a8f549fd2b7718f42a66f59820a016a9eaef
samuel150a <samuelofficial184@gmail.com>
2025-06-19 21:33:24 +0530

    FindById

--- Piano-FindById-Project/src/com/xworkz/piano/dto/PianoDto.java
+++ Piano-FindById-Project/src/com/xworkz/piano/dto/PianoDto.java
@@ -1,6 +1,7 @@
 package com.xworkz.piano.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@Data
 public class PianoDto {
     String customberName;
 
