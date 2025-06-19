364d3bb6916f86844e875d02fd957692f0bbc6f2
samuel150a <samuelofficial184@gmail.com>
2025-06-20 04:11:59 +0530

    Jdbc Project

---
+++ Olympics-Registration Form/src/com/xworkz/olympic/dto/OlympicDto.java
@@ -0,0 +1,27 @@
+package com.xworkz.olympic.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@Data
+public class OlympicDto {
+
+
+
+
+
+
+    String memberName;
+    String dob;
+    String gender;
+    String phone;
+    String state;
+    String city;
+    String sport;
+
+
+}
