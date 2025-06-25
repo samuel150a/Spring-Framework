f0328bd2f3ccadd09d5d50d3023f65fcfe4e6bf9
samuel150a <samuelofficial184@gmail.com>
2025-06-26 01:27:19 +0530

    collection framework

---
+++ CollectionFramework/src/com/xworkz/in/runner/LaptopRunner.java
@@ -0,0 +1,37 @@
+package com.xworkz.in.runner;
+
+import com.xworkz.in.dto.LaptopDto;
+import com.xworkz.in.repository.LaptopRepository;
+import com.xworkz.in.repository.LaptopRepositoryImple;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import static com.sun.org.apache.xml.internal.security.keys.keyresolver.KeyResolver.iterator;
+
+public class LaptopRunner {
+    public static void main(String[] args) {
+
+        LaptopRepository repo = new LaptopRepositoryImple();
+        Collection<LaptopDto> collection = repo.findAll();
+        for (LaptopDto laptopDto : collection) {
+            if (laptopDto.isIstouchDisplay()) {
+                System.out.println("brand" + laptopDto.getBrand());
+                System.out.println("Cost: " + laptopDto.getCost());
+                System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55");
+
+            }
+        }
+
+
+        Iterator<LaptopDto> iterator = collection.iterator();
+
+            while (iterator.hasNext()) {
+                LaptopDto la = iterator.next();
+                System.out.println("list  " + la);
+                System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55");
+            }
+
+
+    }
+}
