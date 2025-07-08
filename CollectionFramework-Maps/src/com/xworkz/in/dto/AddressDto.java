0ac0c6c44833fe76b67fd0114927bf859b48fd71
samuel150a <samuelofficial184@gmail.com>
2025-07-09 02:22:09 +0530

    CollectionFrameWork-Maps

---
+++ CollectionFramework-Maps/src/com/xworkz/in/dto/AddressDto.java
@@ -0,0 +1,19 @@
+package com.xworkz.in.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+public class AddressDto {
+    private int no;
+    private String street;
+    private String city;
+    private int pincode;
+
+
+}
