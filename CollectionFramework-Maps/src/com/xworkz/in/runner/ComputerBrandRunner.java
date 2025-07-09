aa8de0e848c41cb719b0f76775cbe354b7f90a2c
samuel150a <samuelofficial184@gmail.com>
2025-07-10 01:58:10 +0530

    CollectionFrameWork-Maps

--- CollectionFramework-Maps/src/com/xworkz/in/runner/ComputerBrandRunner.java
+++ CollectionFramework-Maps/src/com/xworkz/in/runner/ComputerBrandRunner.java
@@ -6,14 +6,23 @@ import com.xworkz.in.repository.ComputerBrandRepository;
 import com.xworkz.in.repository.ComputerBrandRepositoryImple;
 
 import java.util.Map;
+import java.util.Set;
 
 public class ComputerBrandRunner
 {
     public static void main(String[] args) {
         ComputerBrandRepository computerBrandRepository=new ComputerBrandRepositoryImple();
         Map<ComputerDto, BrandDto> map=computerBrandRepository.findAll();
-        map.keySet().forEach(System.out::println);
+        /*map.keySet().forEach(System.out::println);
         System.out.println("##########################33");
-        map.values().forEach(System.out::println);
+        map.values().forEach(System.out::println);*/
+        Set<Map.Entry<ComputerDto,BrandDto>> entryset=map.entrySet();
+        entryset.forEach(c->
+        {
+            System.out.println(c.getKey());
+            System.out.println(c.getValue());
+        });
+
+
     }
 }
