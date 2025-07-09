5c2219fa895855f9e489d022529820a20e07c4f5
samuel150a <samuelofficial184@gmail.com>
2025-07-10 01:59:03 +0530

    CollectionFrameWork-Maps

---
+++ CollectionFramework-Maps/src/com/xworkz/in/dto/PersonEducationMap.java
@@ -0,0 +1,17 @@
+package com.xworkz.in.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+
+public class PersonEducationMap
+{
+    private String name;
+    private  String stream;
+}
