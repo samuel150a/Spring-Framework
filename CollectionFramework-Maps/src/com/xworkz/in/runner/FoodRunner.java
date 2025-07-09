4b5093d621337815533cda28257deed858a982a1
samuel150a <samuelofficial184@gmail.com>
2025-07-10 01:58:42 +0530

    CollectionFrameWork-Maps

--- CollectionFramework-Maps/src/com/xworkz/in/runner/FoodRunner.java
+++ CollectionFramework-Maps/src/com/xworkz/in/runner/FoodRunner.java
@@ -6,13 +6,21 @@ import com.xworkz.in.repository.FoodRepositoryImple;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class FoodRunner {
     public static void main(String[] args) {
         FoodRepository foodRepository=new FoodRepositoryImple();
         Map<FoodDto, List<String>> map=foodRepository.findAll();
-        map.keySet().forEach(System.out::println);
+       /* map.keySet().forEach(System.out::println);
         System.out.println("****************************************************");
-        map.values().forEach(System.out::println);
+        map.values().forEach(System.out::println);*/
+
+        Set<Map.Entry<FoodDto,List<String>>> entryset=map.entrySet();
+        entryset.forEach(c->{
+            System.out.println(c.getKey());
+            System.out.println(c.getValue());});
+
+
     }
 }
