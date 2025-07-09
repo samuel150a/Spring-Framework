b893ad9a475cb3c19f023443fd74df27a509ee3b
samuel150a <samuelofficial184@gmail.com>
2025-07-09 11:03:50 +0530

    CollectionFrameWork-Maps

---
+++ CollectionFramework-Maps/src/com/xworkz/in/runner/CourseRunner.java
@@ -0,0 +1,21 @@
+package com.xworkz.in.runner;
+
+import com.xworkz.in.dto.CourseDto;
+import com.xworkz.in.repository.CourseRepository;
+import com.xworkz.in.repository.CourseRepositoryImple;
+
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class CourseRunner {
+
+    public static void main(String[] args) {
+        CourseRepository courseRepository=new CourseRepositoryImple();
+        Map<CourseDto,String[]> map =courseRepository.findAll();
+
+        map.keySet().forEach(System.out::println);
+        System.out.println("****************************************************");
+        map.values().forEach(e->System.out.println(Arrays.toString(e)));
+    }
+}
