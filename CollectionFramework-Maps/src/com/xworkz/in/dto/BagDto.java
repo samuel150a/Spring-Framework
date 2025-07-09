fffc72ea8906319075c69ef3a0f99341cd595fd5
samuel150a <samuelofficial184@gmail.com>
2025-07-09 12:03:38 +0530

    CollectionFrameWork-Maps

---
+++ CollectionFramework-Maps/src/com/xworkz/in/dto/BagDto.java
@@ -0,0 +1,14 @@
+package com.xworkz.in.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class BagDto {
+    private Integer bagId;
+    private String type;
+
+}
