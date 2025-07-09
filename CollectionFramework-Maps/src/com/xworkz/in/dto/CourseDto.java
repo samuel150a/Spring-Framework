cc178e1e53dba6b090ca7dbaa481e02161083c14
samuel150a <samuelofficial184@gmail.com>
2025-07-09 11:03:17 +0530

    CollectionFrameWork-Maps

---
+++ CollectionFramework-Maps/src/com/xworkz/in/dto/CourseDto.java
@@ -0,0 +1,38 @@
+package com.xworkz.in.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class CourseDto
+{
+    private Integer courseId;
+    private String courseName;
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(courseName);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj!=null)
+        {
+            if(obj instanceof CourseDto)
+            {
+                CourseDto casting=(CourseDto) obj;
+                return this.courseId.equals(casting.courseId);
+
+            }
+            return  false;
+        }
+        return super.equals(obj);
+    }
+}
