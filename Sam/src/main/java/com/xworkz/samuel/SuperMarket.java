71a1bd68e9a913b4e3079e626e8040bb7c4b030b
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:32:35 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/SuperMarket.java
@@ -0,0 +1,11 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+@Component
+
+public class SuperMarket {
+    public SuperMarket()
+    {
+        System.out.println("Running in SuperMarket");
+    }
+}
