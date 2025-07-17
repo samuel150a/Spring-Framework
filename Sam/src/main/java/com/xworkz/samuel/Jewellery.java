985441e0ce276f4c9524d41972a57203fa028888
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:29:59 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Jewellery.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Jewellery {
+    public Jewellery()
+    {
+        System.out.println("running in Jewellery");
+    }
+}
