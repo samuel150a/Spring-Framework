5c65800ef7f6d58dfb91a6e4f1cd4d71af2c6313
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:32:10 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Spices.java
@@ -0,0 +1,11 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Spices {
+    public Spices() {
+        System.out.println("Running in the Spices");
+    }
+}
