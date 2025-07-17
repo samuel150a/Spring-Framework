345cacce519550e958308633424d48fdee82fc62
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:33:22 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Travel.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Travel {
+    public Travel()
+    {
+        System.out.println("running in the Travel");
+    }
+}
