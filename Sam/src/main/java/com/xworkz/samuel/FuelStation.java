4802d23ab1327c47942df6aa479f8d9554e26526
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:28:27 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/FuelStation.java
@@ -0,0 +1,11 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FuelStation {
+    public FuelStation()
+    {
+        System.out.println("Station is created");
+    }
+}
