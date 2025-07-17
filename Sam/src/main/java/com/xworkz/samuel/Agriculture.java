a51384f8ea2616b20067f0b4100795451df77c5e
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:25:59 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Agriculture.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Agriculture {
+    public Agriculture()
+    {
+        System.out.println("Running in Agriculture");
+    }
+}
