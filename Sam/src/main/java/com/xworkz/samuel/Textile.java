b9fecd0e4e252561c6d4da968bf2dcaf595f813e
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:32:53 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Textile.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Textile {
+    public Textile()
+    {
+        System.out.println("running in the Textile");
+    }
+}
