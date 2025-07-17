03b0566ed241c9b6c038d9607ba377e7aba03f09
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:27:01 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Coconut.java
@@ -0,0 +1,13 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Coconut
+{
+    public Coconut()
+    {
+        System.out.println("running in Coconut");
+    }
+}
