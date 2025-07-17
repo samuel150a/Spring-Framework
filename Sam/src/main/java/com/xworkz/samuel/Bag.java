bb6a72792fb13ac9f4c0aaea173059035078666f
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:26:13 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Bag.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Bag {
+    public Bag()
+    {
+        System.out.println("Running in the Bag");
+    }
+}
