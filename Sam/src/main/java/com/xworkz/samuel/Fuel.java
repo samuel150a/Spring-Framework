7a48fb76c2cbff9cb4c686c257c4b4e30e2f2c87
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:28:17 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Fuel.java
@@ -0,0 +1,11 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Fuel {
+    public Fuel()
+    {
+        System.out.println("running in fuel");
+    }
+}
