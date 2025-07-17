d56316a0539b08d2f91984ac394aacdcb4801a50
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:30:38 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Manufacturing.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Manufacturing {
+    public Manufacturing()
+    {
+        System.out.println("running in the Manufacturing ");
+    }
+}
