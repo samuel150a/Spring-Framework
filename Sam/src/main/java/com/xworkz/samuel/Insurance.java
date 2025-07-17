e1588050ec3658791e82e3ad321b5b9041b0e1b4
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:29:43 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Insurance.java
@@ -0,0 +1,13 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Insurance {
+    public Insurance()
+    {
+        System.out.println("running in the Insurance ");
+
+    }
+}
