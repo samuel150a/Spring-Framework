7c0db2b9c672ddb778d2e195dfb07b6094dfbe1d
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:26:48 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/BookStore.java
@@ -0,0 +1,13 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class BookStore
+{
+    public BookStore()
+    {
+        System.out.println("running in the BookStore");
+    }
+}
