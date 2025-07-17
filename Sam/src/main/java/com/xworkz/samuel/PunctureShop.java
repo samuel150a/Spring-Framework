d8e8f2fbd24ee94043e6374a6455b6f6dabdec27
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:31:25 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/PunctureShop.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class PunctureShop {
+    public PunctureShop()
+    {
+        System.out.println("running in the PunctureShop");
+    }
+}
