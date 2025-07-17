44d19f67cec9ddee8f818d3c2e192662cac27389
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:26:25 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Bakery.java
@@ -0,0 +1,13 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Bakery
+{
+    public  Bakery()
+    {
+        System.out.println("Running in the Bakery ");
+    }
+}
