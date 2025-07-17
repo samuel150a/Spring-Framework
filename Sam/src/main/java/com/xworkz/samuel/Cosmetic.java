aa845d5ca88538596dfe0effd5f54108ed6a2137
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:27:37 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Cosmetic.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Cosmetic {
+    public Cosmetic()
+    {
+        System.out.println("running in the Cosmetic");
+    }
+}
