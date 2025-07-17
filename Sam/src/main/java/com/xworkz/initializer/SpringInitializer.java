e18011c895f0f990f88bee0e6ee693c0b3663152
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:32:23 +0530

    Welcome To SpringFramework

--- Sam/src/main/java/com/xworkz/initializer/SpringInitializer.java
+++ Sam/src/main/java/com/xworkz/initializer/SpringInitializer.java
@@ -1,5 +1,6 @@
-package com.xworkz.Sam;
+package com.xworkz.initializer;
 
+import com.xworkz.samuel.Configure;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class SpringInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -12,7 +13,8 @@ public class SpringInitializer extends AbstractAnnotationConfigDispatcherServlet
     @Override
     protected Class<?>[] getServletConfigClasses() {
         System.out.println("running in getServletConfigClasses  ");
-        return new Class[0];
+        return new Class[] { Configure.class };
+
     }
 
     @Override
