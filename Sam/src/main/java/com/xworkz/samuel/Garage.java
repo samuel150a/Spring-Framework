9aa4147e616f9c96ad09e81296038a4f285338e3
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:28:40 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Garage.java
@@ -0,0 +1,13 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Garage {
+
+    public Garage()
+    {
+        System.out.println("running in Garage");
+    }
+}
