5033f9acdc1deb2a47b94adca65990e09f19157d
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:31:10 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Oil.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Oil {
+    public Oil()
+    {
+        System.out.println("running in Oil");
+    }
+}
