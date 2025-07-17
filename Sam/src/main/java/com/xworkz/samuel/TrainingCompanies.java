1397b82ca2584bcfcc24734fcbd0363501639c86
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:33:07 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/TrainingCompanies.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class TrainingCompanies {
+    public TrainingCompanies()
+    {
+        System.out.println("running in the TrainingCompanies");
+    }
+}
