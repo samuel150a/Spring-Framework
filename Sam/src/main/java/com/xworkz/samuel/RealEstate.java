5773264dd6b7453195a5241b598009da6e922f4a
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:31:49 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/RealEstate.java
@@ -0,0 +1,11 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class RealEstate {
+    public RealEstate(){
+        System.out.println("running in the RealEstate");
+    }
+}
