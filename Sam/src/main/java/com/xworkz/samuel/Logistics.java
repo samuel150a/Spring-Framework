8e16bd87f529175624c1b543c664805a44b0511b
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:30:20 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Logistics.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Logistics {
+    public Logistics()
+    {
+        System.out.println("running in the Logistics");
+    }
+}
