c18686d5e5d8a972f6f63de9278ad9d617ce4db6
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:27:27 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Construction.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Construction {
+    public Construction()
+    {
+        System.out.println("Running in Construction");
+    }
+}
