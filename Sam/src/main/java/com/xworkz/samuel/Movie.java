a3bf1ca07e79d912586eb19a6b17ee80a3e6b44d
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:30:53 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Movie.java
@@ -0,0 +1,11 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Movie {
+    public Movie(){
+        System.out.println("running in the movie");
+    }
+}
