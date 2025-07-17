553cb9dde20864b2877ae535ba3b9c1bc3b95f91
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:29:07 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Hotel.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Hotel {
+    public Hotel()
+    {
+        System.out.println("Running in the hotel");
+    }
+}
