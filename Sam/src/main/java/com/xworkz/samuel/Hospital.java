78b3fe6ca4f9ecfebcd82bb49bed7ebcfd6fd627
samuel150a <samuelofficial184@gmail.com>
2025-07-17 10:28:54 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/samuel/Hospital.java
@@ -0,0 +1,12 @@
+package com.xworkz.samuel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Hospital {
+    public Hospital()
+    {
+        System.out.println("running in the Hospital ");
+    }
+}
