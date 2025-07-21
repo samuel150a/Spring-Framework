f65470ad5e09bdd23e06f80ee25e4e18ff7a72b9
samuel150a <samuelofficial184@gmail.com>
2025-07-21 10:35:19 +0530

    Welcome To SpringFramework

---
+++ Sam/src/main/java/com/xworkz/internal/Laptop.java
@@ -0,0 +1,14 @@
+package com.xworkz.internal;
+
+public class Laptop {
+
+
+    String name;
+    int price;
+
+    public Laptop(String name,int price)
+    {
+        this.name=name;
+        this.price=price;
+    }
+}
