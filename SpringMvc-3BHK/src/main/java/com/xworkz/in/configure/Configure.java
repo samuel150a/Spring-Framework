7706f991432f8326698817faf2a53075bd23814b
samuel150a <samuelofficial184@gmail.com>
2025-07-21 22:33:57 +0530

    All about SpringAnnotations

---
+++ SpringMvc-3BHK/src/main/java/com/xworkz/in/configure/Configure.java
@@ -0,0 +1,12 @@
+package com.xworkz.in.configure;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+@Configuration
+@ComponentScan("com.xworkz.in.Runner")
+
+public class Configure {
+
+}
+
