24d8c17c64f42fbece2a9057d684e516fae8f220
samuel150a <samuelofficial184@gmail.com>
2025-07-21 22:34:48 +0530

    All about SpringAnnotations

---
+++ SpringMvc-3BHK/src/main/java/com/xworkz/in/Runner/Runner.java
@@ -0,0 +1,25 @@
+package com.xworkz.in.Runner;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class Runner {
+
+    public Runner()
+    {
+        System.out.println("runnning in the runner constructor");
+    }
+    @RequestMapping("/Open")
+
+    public String Price()
+    {
+        System.out.println(" 3-BHK rent is 40000");
+        return "/Success.jsp";
+    }
+
+
+
+}
