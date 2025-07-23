f9bdcdefdd83f4f8909071d486b29533b238b11e
samuel150a <samuelofficial184@gmail.com>
2025-07-23 23:49:18 +0530

    SpringMvc

--- FrontControllerDesignPattern-Diary/src/main/java/com/xworkz/diary/controller/NameController.java
+++ FrontControllerDesignPattern-Diary/src/main/java/com/xworkz/diary/controller/NameController.java
@@ -1,10 +1,12 @@
 package com.xworkz.diary.controller;
 
+import com.xworkz.diary.dto.NameDto;
+import com.xworkz.diary.dto.RTODto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+/*1*/
 @Controller
 @RequestMapping("/")
 public class NameController
@@ -13,10 +15,14 @@ public class NameController
     {
         System.out.println("NameController Constructor");
     }
+
     @RequestMapping("/name")
-    public String Idisplay(@RequestParam String firstName, @RequestParam String middleName, @RequestParam String lastName,@RequestParam String displayName,Model model)
+    public String Idisplay(NameDto nameDto,Model model)
     {
-        System.out.println("Name form method");
+
+        System.out.println(nameDto);
+        model.addAttribute("nameDto",nameDto);
+       /* System.out.println("Name form method");
         System.out.println("First Name: "+firstName);
         System.out.println("Middle Name: "+middleName);
         System.out.println("Last Name: "+lastName);
@@ -25,7 +31,7 @@ public class NameController
         model.addAttribute("firstName",firstName);
         model.addAttribute("lastName",lastName);
         model.addAttribute("middleName",middleName);
-        model.addAttribute("displayName",displayName);
+        model.addAttribute("displayName",displayName);*/
 
         return "/NameResult.jsp";
 
