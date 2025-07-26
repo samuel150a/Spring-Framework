a6d2bb5a70e5a0e44deb5df7b6b4590c32660275
samuel150a <samuelofficial184@gmail.com>
2025-07-26 23:21:00 +0530

    SpringMvc

--- Ice-Cream-Shop/src/main/java/com/xworkz/in/service/IceCreamShopService.java
+++ Ice-Cream-Shop/src/main/java/com/xworkz/in/service/IceCreamShopService.java
@@ -4,4 +4,5 @@ import com.xworkz.in.dto.IceCreamShopDto;
 
 public interface IceCreamShopService {
     boolean verify(IceCreamShopDto iceCreamShopDto);
+    double getPrice(String flavour,int quantity);
 }
