279d1f1381956dca5dbb7fc85128a7d407d25235
samuel150a <samuelofficial184@gmail.com>
2025-07-27 19:28:45 +0530

    Task:1.Configure View Resolver 2.Read data from excel  using poi library

--- Ice-Cream-Shop/src/main/java/com/xworkz/in/service/IceCreamShopService.java
+++ Ice-Cream-Shop/src/main/java/com/xworkz/in/service/IceCreamShopService.java
@@ -5,4 +5,5 @@ import com.xworkz.in.dto.IceCreamShopDto;
 public interface IceCreamShopService {
     boolean verify(IceCreamShopDto iceCreamShopDto);
     double getPrice(String flavour,int quantity);
+    double getDiscountedPrice(IceCreamShopDto dto);
 }
