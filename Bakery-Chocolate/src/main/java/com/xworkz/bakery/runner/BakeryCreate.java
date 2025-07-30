907775f080c2bb6ceeed8817050d6a9782d6a122
samuel150a <samuelofficial184@gmail.com>
2025-07-30 22:27:09 +0530

    CRUD Operation Using JPA

---
+++ Bakery-Chocolate/src/main/java/com/xworkz/bakery/runner/BakeryCreate.java
@@ -0,0 +1,42 @@
+package com.xworkz.bakery.runner;
+
+import javax.persistence.*;
+
+public class BakeryCreate {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory=null;
+        EntityManager entityManager=null;
+        EntityTransaction entityTransaction=null;
+
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("x-workz");//load,register,connection
+            entityManager = entityManagerFactory.createEntityManager();// dml(ins,up,del),DQL(select)
+            entityTransaction = entityManager.getTransaction();//rollback,autocommit
+            entityTransaction.begin();
+
+
+            entityTransaction.commit();
+
+
+        } catch (PersistenceException e) {
+            // System.out.println(e.getMessage());
+
+            e.printStackTrace();
+            if(entityTransaction!=null) {
+                entityTransaction.rollback();
+            }
+        } finally {
+            if(entityManagerFactory!=null)
+            {
+                entityManagerFactory.close();
+            }
+            if(entityManager!=null)
+            {
+                entityManager.close();
+            }
+
+
+
+        }
+    }
+}
