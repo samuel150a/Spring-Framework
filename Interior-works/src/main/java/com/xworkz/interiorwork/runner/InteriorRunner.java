1781973cedbf7aaabcab3c1b42f3862d527123b0
samuel150a <samuelofficial184@gmail.com>
2025-08-02 23:52:27 +0530

    MVC Architecture using JPA

---
+++ Interior-works/src/main/java/com/xworkz/interiorwork/runner/InteriorRunner.java
@@ -0,0 +1,34 @@
+package com.xworkz.interiorwork.runner;
+
+import com.xworkz.interiorwork.entity.InteriorEntity;
+import com.xworkz.interiorwork.service.InteriorService;
+import com.xworkz.interiorwork.service.InteriorServiceImple;
+
+import javax.persistence.*;
+
+public class InteriorRunner {
+    public static void main(String[] args) {
+        InteriorEntity interiorEntity1 = new InteriorEntity();
+
+        interiorEntity1.setShop("Design me");
+        interiorEntity1.setWood(true);
+        interiorEntity1.setPrice(500);
+        interiorEntity1.setDoordelivery("no");
+        interiorEntity1.setVariety("office");
+        interiorEntity1.setType("wood");
+
+        InteriorService interiorService = new InteriorServiceImple();
+        //interiorService.validation(interiorEntity1);
+       // interiorService.savedInterior(interiorEntity1);
+
+//        InteriorEntity ref = interiorService.readInterior(interiorEntity1);
+//        System.out.println(("the list of some readed data from Database"));
+//        System.out.println(ref);
+
+       //interiorService.updateInterior(interiorEntity1);
+        interiorService.deleteInterior(interiorEntity1);
+    }
+}
+
+
+
