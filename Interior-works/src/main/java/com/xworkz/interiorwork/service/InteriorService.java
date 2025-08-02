4f58de865a0f4ae7640c2ec4afbb4876b738bef9
samuel150a <samuelofficial184@gmail.com>
2025-08-02 23:52:37 +0530

    MVC Architecture using JPA

---
+++ Interior-works/src/main/java/com/xworkz/interiorwork/service/InteriorService.java
@@ -0,0 +1,11 @@
+package com.xworkz.interiorwork.service;
+
+import com.xworkz.interiorwork.entity.InteriorEntity;
+
+public interface InteriorService {
+    InteriorEntity validation(InteriorEntity en);
+    public void savedInterior(InteriorEntity entity);
+    public InteriorEntity readInterior(InteriorEntity entity);
+    public InteriorEntity updateInterior(InteriorEntity entity);
+    public InteriorEntity deleteInterior(InteriorEntity entity);
+}
