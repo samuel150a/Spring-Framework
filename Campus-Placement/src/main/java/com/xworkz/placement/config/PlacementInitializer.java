c99cb1f0f1dcaad6c85e43d8b68d491de850c8f3
samuel150a <samuelofficial184@gmail.com>
2025-08-04 22:41:06 +0530

    File Saving

---
+++ Campus-Placement/src/main/java/com/xworkz/placement/config/PlacementInitializer.java
@@ -0,0 +1,46 @@
+package com.xworkz.placement.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+import java.io.File;
+
+public class PlacementInitializer extends AbstractAnnotationConfigDispatcherServletInitializer implements WebMvcConfigurer {
+
+    public PlacementInitializer()
+    {
+        System.out.println("PlacementInitializer constructor");
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{PlacementConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+
+        File uploadDirectory = new File(System.getProperty("java.io.tmpdir"));
+
+        int maxUploadSizeInMb = 5 * 1024 * 1024;
+        MultipartConfigElement multipartConfigElement =
+                new MultipartConfigElement(uploadDirectory.getAbsolutePath(),
+                        maxUploadSizeInMb, maxUploadSizeInMb * 2, maxUploadSizeInMb / 2);
+
+        registration.setMultipartConfig(multipartConfigElement);
+
+    }
+
+}
