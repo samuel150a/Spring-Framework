d7c2fd85fa32e2d1dd5444c9c3a862ea50fd47e8
samuel150a <samuelofficial184@gmail.com>
2025-08-05 00:39:14 +0530

    Fetching Data From getSingleResult() & getResultList()

--- Interior-works/src/main/java/com/xworkz/interiorwork/service/InteriorService.java
+++ Interior-works/src/main/java/com/xworkz/interiorwork/service/InteriorService.java
@@ -2,17 +2,27 @@ package com.xworkz.interiorwork.service;
 
 import com.xworkz.interiorwork.entity.InteriorEntity;
 
+import java.util.List;
+
 public interface InteriorService {
     InteriorEntity validation(InteriorEntity en);
     public void savedInterior(InteriorEntity entity);
     public InteriorEntity readInterior(InteriorEntity entity);
     public InteriorEntity updateInterior(InteriorEntity entity);
     public InteriorEntity deleteInterior(InteriorEntity entity);
+    List<InteriorEntity> fetchAllInterior(InteriorEntity entity);
     public InteriorEntity getInteriorEntityByName(String shop);
     public InteriorEntity getInteriorEntityBytype(String type);
     public InteriorEntity getInteriorEntityByvariety(String variety);
     public InteriorEntity getInteriorEntityBywood(boolean wood );
+
+
+
+    //4 methods
+    List<InteriorEntity> getBySpecifictype(String entity);
+    List<InteriorEntity> getBYSpecificvariety(String entity);
     public InteriorEntity getInteriorEntityByPrice(Integer price);
     public InteriorEntity getInteriorEntityBydoordelivery(String doordelivery);
+
 
 }
