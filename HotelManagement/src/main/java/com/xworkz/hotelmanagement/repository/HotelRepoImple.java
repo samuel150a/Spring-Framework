ea2e2f1fc4e5a500ed6c3c298e674e955c039c95
samuel150a <samuelofficial184@gmail.com>
2025-08-05 08:08:09 +0530

    JPA

---
+++ HotelManagement/src/main/java/com/xworkz/hotelmanagement/repository/HotelRepoImple.java
@@ -0,0 +1,247 @@
+package com.xworkz.hotelmanagement.repository;
+
+import com.xworkz.hotelmanagement.entity.HotelEntity;
+
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.List;
+
+public class HotelRepoImple implements HotelRepo {
+    public HotelRepoImple() {
+        System.out.println("running in the HotelRepoImple constructor");
+    }
+
+    EntityManagerFactory entityManagerFactory = null;
+    EntityManager entityManager = null;
+    EntityTransaction entityTransaction = null;
+
+    @Override
+    public void savedHotel(HotelEntity entity) {
+        System.out.println("running in saveHotel method repoimple");
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("x-workz");
+            entityManager = entityManagerFactory.createEntityManager();
+            entityTransaction = entityManager.getTransaction();
+
+            entityTransaction.begin();
+            if (entity != null) {
+                entityManager.persist(entity);
+                System.out.println("Data is saved in DataBase");
+            } else {
+                System.out.println("Entity is null for save operation");
+            }
+            entityTransaction.commit();
+        } catch (PersistenceException e) {
+            System.out.println("Some Exception is occurred");
+            System.out.println(e.getMessage());
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+            }
+        } finally {
+            if (entityManagerFactory != null) entityManagerFactory.close();
+            if (entityManager != null) entityManager.close();
+        }
+    }
+
+    @Override
+    public HotelEntity readHotel(HotelEntity entity) {
+        System.out.println("running in the readHotel method in repoimple class ");
+        HotelEntity result = null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("x-workz");
+            entityManager = entityManagerFactory.createEntityManager();
+            entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            result = entityManager.find(HotelEntity.class, 1);
+        } catch (PersistenceException e) {
+            System.out.println(e.getMessage());
+            if (entityTransaction != null) entityTransaction.rollback();
+        } finally {
+            if (entityManagerFactory != null) entityManagerFactory.close();
+            if (entityManager != null) entityManager.close();
+        }
+        return result;
+    }
+
+    @Override
+    public HotelEntity updateHotel(HotelEntity entity) {
+        System.out.println("running in updateHotel in repoimple class");
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("x-workz");
+            entityManager = entityManagerFactory.createEntityManager();
+            entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            HotelEntity update = entityManager.find(HotelEntity.class, 1);
+            if (update != null) {
+                update.setFeedback("updated feedback");
+            }
+            entityTransaction.commit();
+        } catch (PersistenceException e) {
+            System.out.println(e.getMessage());
+            if (entityTransaction != null) entityTransaction.rollback();
+        } finally {
+            if (entityManagerFactory != null) entityManagerFactory.close();
+            if (entityManager != null) entityManager.close();
+        }
+        return null;
+    }
+
+    @Override
+    public HotelEntity deleteHotel(HotelEntity entity) {
+        System.out.println("running in the deleteHotel method");
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("x-workz");
+            entityManager = entityManagerFactory.createEntityManager();
+            entityTransaction = entityManager.getTransaction();
+            HotelEntity found = entityManager.find(HotelEntity.class, 1);
+            if (found != null) {
+                entityTransaction.begin();
+                entityManager.remove(found);
+                entityTransaction.commit();
+            }
+        } catch (PersistenceException e) {
+            System.out.println(e.getMessage());
+            if (entityTransaction != null) entityTransaction.rollback();
+        } finally {
+            if (entityManagerFactory != null) entityManagerFactory.close();
+            if (entityManager != null) entityManager.close();
+        }
+        return entity;
+    }
+
+    @Override
+    public HotelEntity getHotelEntityByName(String name) {
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("x-workz");
+            entityManager = entityManagerFactory.createEntityManager();
+            return (HotelEntity) entityManager.createNamedQuery("name").setParameter("name", name).getSingleResult();
+        } finally {
+            if (entityManagerFactory != null) entityManagerFactory.close();
+            if (entityManager != null) entityManager.close();
+        }
+    }
+
+
+
+
+    @Override
+    public HotelEntity getHotelEntityByBill(boolean bill) {
+        HotelEntity hotelEntityByBill = null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("x-workz");
+            entityManager = entityManagerFactory.createEntityManager();
+            Query query = entityManager.createNamedQuery("getbill");
+            hotelEntityByBill = (HotelEntity) query.setParameter("bill", bill).getSingleResult();
+            System.out.println("fetched by hotelEntityByBill false " + hotelEntityByBill);
+            return null;
+        } catch (PersistenceException e) {
+            System.out.println("Exception has been found");
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+                System.out.println("Due to Exception rollback is done");
+            }
+        } finally {
+            if (entityManagerFactory != null) {
+                entityManagerFactory.close();
+                System.out.println("entityManagerFactory is closed");
+
+            }
+            if (entityManager != null) {
+                entityManager.close();
+                System.out.println("entityManager is closed");
+
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public HotelEntity getHotelEntityByFeedback(String feedback) {
+        HotelEntity entityByFeedback = null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("x-workz");
+            entityManager = entityManagerFactory.createEntityManager();
+            Query query = entityManager.createNamedQuery("getfeedback");
+            entityByFeedback = (HotelEntity) query.setParameter("feedback", feedback).getSingleResult();
+            System.out.println("the fetched value getHotelEntityByFeedback " + entityByFeedback);
+
+            return null;
+
+        } catch (PersistenceException e) {
+            System.out.println("Exception has been found");
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+                System.out.println("Due to Exception rollback is done");
+            }
+        } finally {
+            if (entityManagerFactory != null) {
+                entityManagerFactory.close();
+                System.out.println("entityManagerFactory is closed");
+
+            }
+            if (entityManager != null) {
+                entityManager.close();
+                System.out.println("entityManager is closed");
+
+
+            }
+        }
+        return null;
+
+    }
+
+    @Override
+    public List<HotelEntity> getHotelEntityByDish(String dish) {
+        List<HotelEntity> hotelEntity = null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("x-workz");
+            entityManager = entityManagerFactory.createEntityManager();
+
+            Query query = entityManager.createNamedQuery("getDish");
+            hotelEntity = query.getResultList();
+            return hotelEntity;
+        } catch (PersistenceException e) {
+            System.out.println("Exception has been found");
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+                System.out.println("Due to Exception rollback is done");
+            }
+        } finally {
+            if (entityManagerFactory != null) entityManagerFactory.close();
+            if (entityManager != null) entityManager.close();
+        }
+        return hotelEntity;
+    }
+
+    @Override
+    public List<HotelEntity> getHotelEntityByPrice(Integer price) {
+        List<HotelEntity> hotelEntityByPrices=null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("x-workz");
+            entityManager = entityManagerFactory.createEntityManager();
+            Query query = entityManager.createNamedQuery("getPrice");
+            hotelEntityByPrices = query.getResultList();
+            return hotelEntityByPrices;
+        }
+            catch (PersistenceException e) {
+                System.out.println("Exception has been found");
+                System.out.println(e.getMessage());
+                //e.printStackTrace();
+                if (entityTransaction != null) {
+                    entityTransaction.rollback();
+                    System.out.println("Due to Exception rollback is done");
+                }
+
+        } finally {
+            if (entityManagerFactory != null) entityManagerFactory.close();
+            if (entityManager != null) entityManager.close();
+        }
+        return hotelEntityByPrices;
+    }
+}
