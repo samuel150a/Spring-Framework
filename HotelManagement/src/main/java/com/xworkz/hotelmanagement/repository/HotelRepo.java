4a9560886a1e2706fc9518a7046119359928354e
samuel150a <samuelofficial184@gmail.com>
2025-08-05 08:07:53 +0530

    JPA

---
+++ HotelManagement/src/main/java/com/xworkz/hotelmanagement/repository/HotelRepo.java
@@ -0,0 +1,22 @@
+package com.xworkz.hotelmanagement.repository;
+
+import com.xworkz.hotelmanagement.entity.HotelEntity;
+
+import java.util.List;
+
+public interface HotelRepo {
+    public void savedHotel(HotelEntity entity);
+    public HotelEntity readHotel(HotelEntity entity);
+    public HotelEntity updateHotel(HotelEntity entity);
+    public HotelEntity deleteHotel(HotelEntity entity);
+    public HotelEntity getHotelEntityByName(String name);
+
+
+    //4methods
+    public HotelEntity getHotelEntityByBill(boolean bill);
+    public HotelEntity getHotelEntityByFeedback(String feedback);
+    public List<HotelEntity> getHotelEntityByDish(String dish);
+    public List<HotelEntity> getHotelEntityByPrice(Integer price);
+
+}
+
