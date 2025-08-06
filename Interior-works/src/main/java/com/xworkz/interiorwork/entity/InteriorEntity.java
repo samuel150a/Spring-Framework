e73e2564758076c7e2ac8dbc479ae6e08c05f496
samuel150a <samuelofficial184@gmail.com>
2025-08-06 23:03:35 +0530

    updateQuery in jpa

--- Interior-works/src/main/java/com/xworkz/interiorwork/entity/InteriorEntity.java
+++ Interior-works/src/main/java/com/xworkz/interiorwork/entity/InteriorEntity.java
@@ -19,19 +19,45 @@ import javax.persistence.*;
 //@NamedQuery(name="shop",query="select a  from InteriorEntity a where a.shop=:shop")
 //@NamedQuery(name="variety",query="select a from InteriorEntity a where a.variety=:variety")
 //@NamedQuery(name="wood",query="select a from InteriorEntity a where a.wood=:wood")
-
+// query = "UPDATE EmployeeEntity e SET e.salary = :salary WHERE e.id = :id AND e.company = :company"
 // 4 named query
-@NamedQuery(name="doordelivery",query = "select a from InteriorEntity a where a.doordelivery=:doordelivery")
 @NamedQuery(name="entitySpecificType",query = "select a from InteriorEntity a where a.type='wood'")
 @NamedQuery(name="entitySpecificvariety",query="select v from InteriorEntity v where v.variety='hotel'")
 @NamedQuery(name="getprice",query ="select z from InteriorEntity z where z.price=:price")
 @NamedQuery(name="entityDoorDelivery",query ="select d from InteriorEntity d where d.doordelivery=:doordelivery")
 
+@NamedQuery(
+        name = "updateById",
+        query = "UPDATE InteriorEntity e SET e.type = :type, e.shop = :shop WHERE e.ID = :ID"
+)
+@NamedQuery(name="updateByPrice",
+        query="UPDATE InteriorEntity p SET p.variety=:variety WHERE p.price=:price "
+      )
+
+
+@NamedQuery(name="updateByShop"
+        ,query="update InteriorEntity s set s.wood=:wood where s.shop=:shop And s.ID=:ID "
+)
+
+@NamedQuery(name="getAllVarietynames"
+        ,query="select v.variety from InteriorEntity v")
+
+@NamedQuery(name="getAllPrice"
+          ,query="select p.price from InteriorEntity p"
+)
+@NamedQuery(name="getAllWood"
+        ,query="select w.wood from InteriorEntity w"
+)
+@NamedQuery(name="FetchTwoColumns"
+        ,query="select i.ID , i.variety from InteriorEntity i"
+)
+
+
 public class InteriorEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="ID")
-    private int ID;
+    private Integer ID;
 
 
     @Column(name="type")
@@ -44,10 +70,10 @@ public class InteriorEntity {
     private String variety;
 
     @Column(name="wood")
-    private boolean wood;
+    private Boolean wood;
 
     @Column(name="price")
-    private int price;
+    private Integer price;
 
     @Column(name="doordelivery")
     private String doordelivery;
