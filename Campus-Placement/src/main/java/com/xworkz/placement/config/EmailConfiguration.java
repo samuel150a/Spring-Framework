c189cc407b8db344fa571ba31c5c1845655a513f
samuel150a <samuelofficial184@gmail.com>
2025-08-07 23:55:35 +0530

    File Saving

---
+++ Campus-Placement/src/main/java/com/xworkz/placement/config/EmailConfiguration.java
@@ -0,0 +1,25 @@
+package com.xworkz.placement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfiguration {
+
+    public  JavaMailSenderImpl  mailSender(){
+        JavaMailSenderImpl javaMailSender=new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+        javaMailSender.setUsername("samuelofficial184@gmail.com");
+        javaMailSender.setPassword("gaxl neys esxn xnot");
+        Properties properties =javaMailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol","smtp");
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+        return javaMailSender;
+    }
+
+}
