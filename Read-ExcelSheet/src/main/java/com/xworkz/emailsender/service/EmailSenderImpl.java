01a813df5d5db8788dce534376011d964d4e402a
samuel150a <samuelofficial184@gmail.com>
2025-08-07 23:57:06 +0530

    File Saving

---
+++ Read-ExcelSheet/src/main/java/com/xworkz/emailsender/service/EmailSenderImpl.java
@@ -0,0 +1,27 @@
+package com.xworkz.emailsender.service;
+
+import com.xworkz.emailsender.config.EmailConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailSenderImpl implements EmailSender {
+
+    @Autowired
+    EmailConfiguration configuration;
+
+
+    @Override
+    public void mailSender(String email, String otp) {
+        System.out.println("Invoking mailSend ");
+        SimpleMailMessage simpleMailMessage= new SimpleMailMessage();
+        simpleMailMessage.setTo(email);
+        simpleMailMessage.setSubject("Otp to login");
+        simpleMailMessage.setText("Otp for login "+otp);
+        configuration.mailSender().send(simpleMailMessage);
+        System.out.println("Otp sent for :"+email+" : "+otp);
+    }
+
+
+}
