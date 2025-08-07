c9d95d3b6e56ab24bb63586c27b09615062dd466
samuel150a <samuelofficial184@gmail.com>
2025-08-07 23:56:44 +0530

    File Saving

---
+++ Read-ExcelSheet/src/main/java/com/xworkz/emailsender/controller/EmilController.java
@@ -0,0 +1,26 @@
+package com.xworkz.emailsender.controller;
+
+import com.xworkz.emailsender.service.EmailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller("/")
+public class EmilController {
+    @Autowired
+    EmailSender emailSender;
+
+     @PostMapping("/sendEmail")
+    private String sendEmail(String name){
+         System.out.println("hitting controller");
+         emailSender.mailSender("samueldrakson184@gmail.com","123456");
+         System.out.println("hlo");
+        return "success.jsp";
+    }
+
+
+
+}
