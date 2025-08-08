5cc26a80a7f88eb878abc6ec98826d71a7f80be0
samuel150a <samuelofficial184@gmail.com>
2025-08-08 20:31:09 +0530

    Fetching Data From getSingleResult() & getResultList()

---
+++ tourism-management/src/main/java/com/xworkz/tourism/dto/TouristDto.java
@@ -0,0 +1,15 @@
+package com.xworkz.tourism.dto;
+
+import lombok.Data;
+
+@Data
+public class TouristDto {
+    private String touristName;
+    private String  destination;
+    private  Integer days;
+    private  Integer fee;
+    private  Integer personCount;
+
+
+
+}
